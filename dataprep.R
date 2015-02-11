2a25c8bfebc3c179c75f399014be5a500b3fef55
fupangpangpang <jason.wfu@gmail.com>
2015-02-10 22:41:40 -0500

    Seriously apply real data

--- dataprep.R
+++ dataprep.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 library("coda")
 library(rjson)
 dir_list = c("01KEN_CXR_AGE_0","01KEN_CXR_AGE_1","02GAM_CXR_AGE_0","02GAM_CXR_AGE_1",
@@ -8,6 +9,34 @@ dir_list1 = substr(dir_list,3,15)
 dir_list2 = substr(dir_list,1,5)
 json.prep = list()
 json.prep.raw = list()
+
+#### a function to match each model's result to the desired order:####
+j=1
+DIR_NPLCM = paste0("C:\\Users\\WFu\\Dropbox (PERCH)\\PQM EXPERIMENT DATASETS\\ARCHIVE\\PQM_RESULTS_20141122\\",dir_list2[j],"\\",dir_list[j])
+model_options  <- dget(paste(DIR_NPLCM,"model_options.txt",sep="/"))
+pathogen_list = c(model_options$pathogen_list,model_options$pathogen_SSonly_list)
+DIR_pathogen_displayorder_lookup <- "C:\\PQ_MODEL\\pathogen_displayorder_lookup.csv"
+pathogen_displayorder_lookup <- read.csv(DIR_pathogen_displayorder_lookup)
+f <- pathogen_displayorder_lookup$Pathogen
+display_order <- as.character(levels(f))[f]
+lookup <- function(disp_order, union_nm){
+  ord_union <- rep(NA,length(union_nm))
+  incre <- 0
+  for (j in seq_along(disp_order)){
+    if (disp_order[j]%in% union_nm){
+      incre <- incre + 1
+      ord_union[incre] <- which(union_nm==disp_order[j])
+    }
+  }
+  list(ord_union,union_nm[ord_union])
+}
+order <- lookup(display_order,pathogen_list)[[1]]
+newlist = lookup(display_order,pathogen_list)[[2]]
+order_new = 33-lookup(pathogen_list,newlist)[[1]]
+
+
+
+#####Prepare raw data#####
 for (j in seq_along(dir_list)) {
   DIR_NPLCM = paste0("C:\\Users\\WFu\\Dropbox (PERCH)\\PQM EXPERIMENT DATASETS\\ARCHIVE\\PQM_RESULTS_20141122\\",dir_list2[j],"\\",dir_list[j])
   
@@ -48,9 +77,23 @@ for (j in seq_along(dir_list)) {
   }
   json.prep[[j]] <- as.array(colMeans(res.ggplots))[order]
   row.names(json.prep[[j]])=NULL
-  json.prep.raw[[j]] = res.ggplots
+  json.prep.raw[[j]] = res.ggplots[order]
 }
+names(json.prep.raw)=dir_list
+n=length(dir_list)
+json.prep.raw[[n+1]] = newlist
+json.prep.raw[[n+2]] = newlist
+json.prep.raw[[n+3]] = c(32:1)
+json.prep.raw[[n+4]] = dir_list
+dir_list1[n+1]= "pathogen"
+dir_list1[n+2]= "abbrev"
+dir_list1[n+3]= "rank"
+dir_list1[[n+4]] = "rawlist"
+names(json.prep.raw)= dir_list
 
+
+
+################# Exhausive list#####
 CI_list = list()
 ## Prepare 2.5% and 97.5% for 101 scenario
 site = c("KEN","GAM","MAL","ZAM","SAF","THA","BAN")
@@ -60,54 +103,25 @@ for (s in c(0:6)) {
     CI_list[[s+1]][[w+1]] = list()
 
     result_w = (json.prep.raw[[2*s+1]]*w+json.prep.raw[[2*s+2]]*(100-w))/100
-    res_w_2.5= as.array(apply(result_w, 2, function(x) quantile(x, .025)))[order]
-    res_w_97.5= as.array(apply(result_w, 2, function(x) quantile(x, .975)))[order]
-    res_w_50= as.array(apply(result_w, 2, function(x) quantile(x, .50)))[order]
-    dist = res_w_97.5-res_w_50
+    res_w_2.5= as.array(apply(result_w, 2, function(x) quantile(x, .025)))
+    res_w_97.5= as.array(apply(result_w, 2, function(x) quantile(x, .975)))
+    res_w_mean= as.array(apply(result_w, 2, mean))
+    res_w_sd= as.array(apply(result_w, 2, sd))
     row.names(res_w_2.5)=NULL
     row.names(res_w_97.5)=NULL
-    row.names(dist)=NULL
+    row.names(res_w_mean)=NULL
+    row.names(res_w_sd)=NULL
 
     CI_list[[s+1]][[w+1]][[1]]=res_w_2.5
     CI_list[[s+1]][[w+1]][[2]]=res_w_97.5
-    CI_list[[s+1]][[w+1]][[3]]=dist
-    names( CI_list[[s+1]][[w+1]])=c("p025","p975","dist")
+    CI_list[[s+1]][[w+1]][[3]]=res_w_mean
+    CI_list[[s+1]][[w+1]][[4]]=res_w_sd
+    names( CI_list[[s+1]][[w+1]])=c("p025","p975","mean","sd")
   }
   names(CI_list[[s+1]])=paste("W",c(0:100),sep="")
 }
 names(CI_list)=site
 
-
-
-
-# a function to match each model's result to the desired order:
-DIR_pathogen_displayorder_lookup <- "C:\\PQ_MODEL\\pathogen_displayorder_lookup.csv"
-pathogen_displayorder_lookup <- read.csv(DIR_pathogen_displayorder_lookup)
-f <- pathogen_displayorder_lookup$Pathogen
-display_order <- as.character(levels(f))[f]
-lookup <- function(disp_order, union_nm){
-  ord_union <- rep(NA,length(union_nm))
-  incre <- 0
-  for (j in seq_along(disp_order)){
-    if (disp_order[j]%in% union_nm){
-      incre <- incre + 1
-      ord_union[incre] <- which(union_nm==disp_order[j])
-    }
-  }
-  list(ord_union,union_nm[ord_union])
-}
-
-#   disp_order <- c("B","E","D","C","F","A")
-#   union_nm   <- c("A","B","C","D","E")
-#   nm_list <-list(c("C","E"),
-#                  c("D","B"),
-#                  c("C","A","E"))
-#   lookup(disp_order,union_nm,nm_list)
-order <- lookup(display_order,pathogen_list)[[1]]
-newlist = lookup(display_order,pathogen_list)[[2]]
-order_new = 33-lookup(pathogen_list,newlist)[[1]]
-
-###
 n=length(dir_list)
 json.prep[[n+1]] = newlist
 json.prep[[n+2]] = newlist
@@ -118,12 +132,16 @@ dir_list1[n+2]= "abbrev"
 dir_list1[n+3]= "rank"
 dir_list1[[n+4]] = "rawlist"
 names(json.prep)= dir_list1
-
-
 json.prep.new = c(json.prep,CI_list)
 
-
+#######Create JSON#########
 request.body <- toJSON(json.prep.new)
+sink("C:\\Users\\WFu\\Google Drive\\PERCH\\2_PQ_MODEL\\PQM_Project\\EtioComp\\perchdata.json")
+cat(request.body)
+sink()
+
+#######Create JSON#########
+request.body <- toJSON(json.prep.raw)
 sink("C:\\Users\\WFu\\Google Drive\\PERCH\\2_PQ_MODEL\\PQM_Project\\EtioComp\\perchdata.json")
 cat(request.body)
 sink()
